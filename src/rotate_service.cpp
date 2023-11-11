972baa7804b7f1f5f66218c7c1200e1459b5ffab
raldermanrobotics <raldermanrobotics@gmail.com>
2023-11-11 19:09:56 +0000

    Seems to work now, will delete and remake checkpoint2 branch

--- src/rotate_service.cpp
+++ src/rotate_service.cpp
@@ -16,10 +16,11 @@
 double roll, pitch, yaw;
 double degree_input;
 double input_radian_convert;
-double kP = 0.5; //Proportional gain
+double kP = 0.05; //Proportional gain
 ros::Publisher pub;
 ros::ServiceServer srv_rotate_by_degrees;
 ros::Subscriber sub;
+bool reached_target = false;
 
 //(const nav_msgs::Odometry::custom_service::Requestg& req, pkg_name::custom_service::Response& res)
 
@@ -30,29 +31,62 @@ void getRotation(const nav_msgs::Odometry::ConstPtr& msg)
     tf::quaternionMsgToTF(orientation, tf_orientation);
     tf::Matrix3x3(tf_orientation).getRPY(roll, pitch, yaw);
     ROS_INFO("Yaw = %.2f", yaw);
+
+    if (!reached_target && std::abs(input_radian_convert - yaw) < 0.1)
+    {
+        geometry_msgs::Twist stop;
+        stop.angular.z = 0.0;
+        pub.publish(stop);
+        reached_target = true;
+    }
 }   
 
 bool rotate_with_degrees(my_rb1_ros::custom_service_messages::Request& req, my_rb1_ros::custom_service_messages::Response& res)
 {
     ROS_INFO("Rotation Callback has been called");
-    //variable relative to the callback, likely need to reset counters or values.
+    reached_target = false;
+    //double check_value_left;
+    //double check_value_right;
+    input_radian_convert = req.degrees * M_PI / 180;
     
     geometry_msgs::Twist turn1;
-    
-
-    if (req.degrees >= 0) //turn left
-    {
-        turn1.angular.z = 1.0;
-        ROS_INFO("the input is telling the robot to turn left");
-    }
-    if (req.degrees < 0) //turn right
-    {
-        turn1.angular.z = -1.0;
-        ROS_INFO("the input is telling the robot to turn right");
-    }
-    ROS_INFO("We are rotating no mater what");
+    turn1.angular.z = kP * (input_radian_convert - yaw);
     pub.publish(turn1);
+    res.success = true;
+    
     return true;
+
+
+    //while (req.degrees >= 0 && yaw <= input_radian_convert) //turn left
+    //{
+    //    ROS_INFO("the input is telling the robot to turn left");
+    //    check_value_left = input_radian_convert - yaw;
+
+    //    while ((ros::ok()) && abs(check_value_left) > 0.1)
+    //    {
+    //    turn1.angular.z = input_radian_convert - yaw;
+    //    pub.publish(turn1);
+    //    }
+    //}
+
+    //while (req.degrees < 0 && yaw >= input_radian_convert) //turn right
+    //{
+        
+    //    ROS_INFO("the input is telling the robot to turn right");
+    //    check_value_right = input_radian_convert - yaw;
+
+    //    while((ros::ok()) && abs(check_value_right) > .01)
+    //    {
+    //    turn1.angular.z = input_radian_convert - yaw;
+    //    pub.publish(turn1);
+    //    ros.sleep()
+    //    }
+//    }
+    
+//    ROS_INFO("Finished the turn");
+//   turn1.angular.z = 0;
+//    pub.publish(turn1);
+//    return true;
 }
 
 
